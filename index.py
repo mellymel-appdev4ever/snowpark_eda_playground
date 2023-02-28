37677620a98db569e8819c0913b73b444e7a6419
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-02-28 15:36:01 -0800

    Update index.py

--- index.py
+++ index.py
@@ -38,7 +38,7 @@ with st.container():
    st.write("This is inside the container")
 
    # You can call any Streamlit command, including custom components:
-   t.line_chart(data=traffic_df, x=traffic_date, y=traffic_volume, width=0, height=0, use_container_width=True)
+   st.line_chart(data=traffic_df, x=traffic_date, y=traffic_volume, width=0, height=0, use_container_width=True)
 
 st.write("This is outside the container")
 
