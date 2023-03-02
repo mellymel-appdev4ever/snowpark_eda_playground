ea33d67f577adb370859b7af14143ab2d80333af
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-02 15:04:45 -0800

    Update index.py

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ with st.sidebar:
             traffic_df = session.sql("select * from usonian_bridges.conformed.v_traffic_by_month order by traffic_month, traffic_direction;").collect()
             pd_traffic_df =  pd.DataFrame(traffic_df)
 
-            traffic_2_df = session.sql("select DATE_FROM_PARTS(traffic_year, traffic_month, traffic_dom) as date,, traffic_volume, traffic_direction from usonian_bridges.raw.tacoma_narrows_traffic;").collect()
+            traffic_2_df = session.sql("select DATE_FROM_PARTS(traffic_year, traffic_month, traffic_dom) as date, traffic_volume, traffic_direction from usonian_bridges.raw.tacoma_narrows_traffic;").collect()
             pd_traffic_2_df =  pd.DataFrame(traffic_2_df)
                 
 st.vega_lite_chart(pd_traffic_df, {
