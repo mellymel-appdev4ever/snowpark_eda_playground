909baa909d15867fbfe18f0f7c4700aa0e0e32b4
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-02 12:02:50 -0800

    Update index.py

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ with st.sidebar:
             session = Session.builder.configs(conn).create()
             s.pressed_first_button = True    
       
-            traffic_df = session.sql("select * from usonian_bridges.conformed.v_traffic_by_month;").collect()
+            traffic_df = session.sql("select * from usonian_bridges.conformed.v_traffic_by_month order by traffic_month, traffic_direction;").collect()
             # Convert Snowflake DF to Pandas DF so Streamlit can use it in charts
             pd_traffic_df =  pd.DataFrame(traffic_df) 
            
@@ -37,18 +37,18 @@ with st.container():
   
    st.write(pd_traffic_df) 
    
-   st.line_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='EASTBOUND_TRAFFIC_VOL')
+   st.line_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='TRAFFIC_VOLUME')
      
-   st.area_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='EASTBOUND_TRAFFIC_VOL')
-   st.bar_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='EASTBOUND_TRAFFIC_VOL')
+   st.area_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='TRAFFIC_VOLUME')
+   st.bar_chart(pd_traffic_df, x='TRAFFIC_MONTH', y='TRAFFIC_VOLUME')
         
    st.vega_lite_chart(pd_traffic_df, {
     'mark': {'type': 'circle', 'tooltip': True},
     'encoding': {
         'x': {'field': 'TRAFFIC_MONTH', 'type': 'quantitative'},
         'y': {'field': 'TRAFFIC_VOLUME', 'type': 'quantitative'},
-        'size': {'field': 'EASTBOUND_TRAFFIC_VOLUME', 'type': 'nominal'},
-        'color': {'field': 'EASTBOUND_TRAFFIC_VOLUME', 'type': 'nominal'},
+        'size': {'field': 'TRAFFIC_VOLUME', 'type': 'nominal'},
+        'color': {'field': 'TRAFFIC_VOLUME', 'type': 'nominal'},
     },
 })
 
@@ -57,10 +57,7 @@ st.write("This is outside the container")
 
         
 st.stop()
-#st.line_chart(data=traffic_df, *, x=traffic_date, y=traffic_volume, width=0, height=0, use_container_width=True)
 
-
-st.stop()
          
   
 
