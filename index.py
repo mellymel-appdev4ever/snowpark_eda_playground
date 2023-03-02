76f284ef31f9624ae6a37286fa5fcb2068191b3f
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-02 15:11:58 -0800

    Update index.py

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ with st.sidebar:
             traffic_df = session.sql("select * from usonian_bridges.conformed.v_traffic_by_month order by traffic_month, traffic_direction;").collect()
             pd_traffic_df =  pd.DataFrame(traffic_df)
 
-            traffic_2_df = session.sql("select DATE_FROM_PARTS(traffic_year, traffic_month, traffic_dom) as date, traffic_hour, traffic_volume, traffic_direction from usonian_bridges.raw.tacoma_narrows_traffic;").collect()
+            traffic_2_df = session.sql("select DATE_FROM_PARTS(traffic_year, traffic_month, traffic_dom) as date, traffic_dow, traffic_hour, traffic_volume, traffic_direction from usonian_bridges.raw.tacoma_narrows_traffic;").collect()
             pd_traffic_2_df =  pd.DataFrame(traffic_2_df)
                 
 st.vega_lite_chart(pd_traffic_df, {
@@ -64,7 +64,7 @@ st.vega_lite_chart(pd_traffic_2_df, {
 'mark': 'point',
 'encoding': {
 'x': {'field': 'TRAFFIC_VOLUME', 'type': 'quantitative', 'scale': {'zero': 'false'}},
-'y': {'field': 'DATE', 'type': 'ordinal', 'scale': {'zero': 'false'}},        
+'y': {'field': 'TRAFFIC_DOW', 'type': 'ordinal', 'scale': {'zero': 'false'}},        
 'color': {'field': 'TRAFFIC_DIRECTION', 'type': 'nominal'},
 'shape': {'field': 'TRAFFIC_DIRECTION', 'type': 'nominal'}
         }
